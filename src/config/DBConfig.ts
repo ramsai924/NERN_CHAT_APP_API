c91cdd6b32668be6341c82986a1428b2955e7b9a
ram.sai.ext@siemens.com <ram.sai.ext@siemens.com>
2023-03-16 21:57:28 +0530

    socket and group changes

--- src/config/DBConfig.ts
+++ src/config/DBConfig.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
 import CONSTANTS from './Constants'
-
+const liveUrl = 'mongodb+srv://ramsai924:nextjschatapp@cluster0.gjdphl1.mongodb.net/?retryWrites=true&w=majority'
 mongoose.set('strictQuery', true)
-mongoose.connect(CONSTANTS.dbURL).then((connection: any) => {
+// mongoose.connect(CONSTANTS.dbURL).then((connection: any) => {
+mongoose.connect(liveUrl).then((connection: any) => {
     console.log(`connected to DB`)
 }).catch((err: any) => {
     console.log('Something went wrong while connecting to DB')
